035d1c9504bf1a990357c3218dcda9dd84296e27
Volodymyr Omelianiuk <ovf.1990@gmail.com>
2019-02-13 16:55:46 +0200

    working on homework-02

--- homework-02/src/routes/router.js
+++ homework-02/src/routes/router.js
@@ -1,10 +1,12 @@
 const mainRoute = require("./main/mainRoute");
 const productRoute = require("./products/products");
 const signupRoute = require("./signup/signup");
+const getProduct = require("./products/get-product");
 
 const router = {
   "/products": productRoute,
   "/signup": signupRoute,
+  "/products/:{id}": getProduct,
   default: mainRoute
 };
 
